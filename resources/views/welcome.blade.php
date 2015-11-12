b23b34893cdba208f1349ef3a2bfe865a75fee78
alkazua <alan.meile@stud.hslu.ch>
2015-11-12 15:15:30 +0100

    Add a simple login layout, as a test.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,26 @@
     <head>
         <title>3source</title>
         {!! HTML::style('css/app.css') !!}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <div class="container">
-            <div class="content">
+        <div id="container">
+            <div id="content">
                 <div class="title">Laravel 5</div>
+                <div id="login">
+                    <form>
+                        <div class="form-group">
+                            <label for="txtUsername">Email address</label>
+                            <input type="text" class="form-control" id="txtUsername" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <label for="txtPassword">Password</label>
+                            <input type="password" class="form-control" id="txtPassword" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-default">Login</button>
+                        <button type="submit" class="btn btn-default">Register</button>
+                    </form>
+                </div>
             </div>
         </div>
     </body>
