4829179a3196bfcab71622b18b74d6607ecc9a71
Mogria <m0gr14@gmail.com>
2015-11-12 09:30:27 +0100

    start script restart docker container if already started

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+docker stop 3source-web 3source-php
+docker rm 3source-web 3source-php
 docker run --privileged=true --name 3source-php --volumes-from 3source-data -d dylanlindgren/docker-laravel-phpfpm
 
 docker run --privileged=true --name 3source-web --volumes-from 3source-data -p 80:80 --link 3source-php:fpm -d dylanlindgren/docker-laravel-nginx  
