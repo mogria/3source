6c906a3f91a96e696a443e4631f0357238a8563d
alkazua <alan.meile@stud.hslu.ch>
2015-11-18 14:29:32 +0100

    Add simple login template.

---
+++ public/js/trailer.js
@@ -0,0 +1,8 @@
+/**
+ * Created by alkazua on 18.11.15.
+ */
+
+$(function() {
+    console.log( "Developer Alan has been busy or just bored.." );
+    //$("#trailer").pan({fps: 30, speed: 2, dir: 'left'});
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,16 @@
     <head>
         <title>3source</title>
         {!! HTML::style('css/app.css') !!}
+        <!-- {{ HTML::script('js/jquery-2.1.4.min.js') }}
+        {{ HTML::script('js/spritely-0.6.8/src/jquery.spritely.js') }}
+        {{ HTML::script('js/trailer.js') }} -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <div id="container">
             <div id="content">
                 <div class="title">Laravel 5</div>
+                <div id="trailer"></div>
                 <div id="login">
                     <form>
                         <div class="form-group">
