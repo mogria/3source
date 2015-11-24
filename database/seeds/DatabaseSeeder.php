7d5237ab70156dd5decb1de9c14fa75fcce10908
Mogria <m0gr14@gmail.com>
2015-11-24 08:28:26 +0100

    add database seeder to create a few users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +15,18 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $users = array(
+            ['name' => 'Mogria', 'email' => 'm0gr14@gmail.com', 'password' => Hash::make('test')],
+            ['name' => 'Alkazua', 'email' => 'test-mail@localhost', 'password' => Hash::make('test')],
+            ['name' => 'Johnny Rotten', 'email' => 'test-mail2@localhost', 'password' => Hash::make('secret')],
+            ['name' => 'Sid Vicious', 'email' => 'test-mail3@localhost', 'password' => Hash::make('secret')]
+        );
+                        
+                // Loop through each user above and create the record for them in the database
+        foreach ($users as $user)
+        {
+            User::create($user);
+        }
 
         Model::reguard();
     }
