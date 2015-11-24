5249d1b6ebbb6b9f455ed5a80db1a9c14f1c3215
Mogria <m0gr14@gmail.com>
2015-11-24 19:51:32 +0100

    add test case for /users api

---
+++ tests/UserAPITest.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\User;
+
+class UserAPITest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected $users;
+
+    public function setUp() {
+        parent::setUp();
+        $this->beginDatabaseTransaction();
+
+        $this->users = [];
+        for($i = 0; $i < 10; $i++) {
+            $user = $this->users[] = factory(User::class)->make();
+            $user->save();
+        }
+    }
+
+    /**
+     * test GET /users
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $response = $this->get('/users');
+        foreach($this->users as $user) {
+            $response->seeJson($user->toArray());
+        }
+    }
+
+    /**
+     * test GET /users/{id}
+     *
+     * @return void
+     */
+    public function testShow() {
+        foreach($this->users as $user) {
+            $response = $this->get('/users/' . $user->id);
+            $response->seeJson($user->toArray());
+        }
+    }
+}
