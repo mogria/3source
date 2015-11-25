8c002951fc22246c772d0f3a24eb66daa6c87481
Mogria <m0gr14@gmail.com>
2015-11-25 18:09:52 +0100

    make DungeonBattle a background command

--- app/Commands/DungeonBattle.php
+++ app/Commands/DungeonBattle.php
@@ -3,10 +3,15 @@
 namespace App\Commands;
 
 use App\Commands\Command;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class DungeonBattle extends Command implements SelfHandling
+class DungeonBattle extends Command implements SelfHandling, ShouldQueue
 {
+    use InteractsWithQueue, SerializesModels;
+
     /**
      * Create a new command instance.
      *
